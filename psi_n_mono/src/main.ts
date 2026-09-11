9b75ffab3505d44a6fd3b10453e007bb75521987
cesardddp <cesardddp@hotmail.com>
2026-09-11 17:16:56 -0300

    refactor: reincicialização do projeto vite-vue, atualização de dependencias e troca do bootstrao por OpenVue
    - Atualização de dependencias e add  `@openvue/themes`, `@pinia/colada`, e atualiza Vue pro 3.5

--- psi_n_mono/src/main.ts
+++ psi_n_mono/src/main.ts
@@ -1,7 +1,19 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { PiniaColada } from '@pinia/colada'
 import './style.css'
 import App from './App.vue'
-import "bootstrap/scss/bootstrap.scss";
-import "bootstrap/dist/js/bootstrap.min.js";
 
-createApp(App).mount('#app')
+import OpenVue from 'openvue/config';
+import Aura from '@openvue/themes/aura';
+
+const app = createApp(App);
+app.use(createPinia());
+app.use(PiniaColada);
+app.use(OpenVue, {
+     theme: {
+        preset: Aura
+    }
+});
+
+app.mount('#app')
--- psi_n_mono/vite.config.ts
+++ psi_n_mono/vite.config.ts
@@ -1,10 +1,20 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from "@vitejs/plugin-vue";
+import { defineConfig, loadEnv } from "vite";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  server:{
-    host:true
-  }
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [vue()],
+    base: "/static/",
+
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL || "http://localhost:5000",
+          changeOrigin: true,
+        },
+      },
+    },
+  };
+});
